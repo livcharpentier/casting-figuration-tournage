f838555827b17705954eaa57b5b698e4849fcc7e
livcharpentier <liv.charp@wanadoo.fr>
2026-07-08 07:27:12 +0200

    Ajout import liste figurants par jour avec creation automatique des fiches

---
+++ migration_11_liste_figurants.sql
@@ -0,0 +1,4 @@
+-- Migration 11 : ajouter "liste_figurants" comme type de document importable pour un film
+alter table film_documents drop constraint if exists film_documents_type_document_check;
+alter table film_documents add constraint film_documents_type_document_check
+  check (type_document in ('bible', 'pdt', 'scenario', 'depouillement', 'liste_figurants'));
